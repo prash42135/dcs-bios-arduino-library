a1c8286f2a70d218d457b1ac5a932666ee7bccd5
prashant <prash42135@gmail.com>
2023-03-06 22:45:02 -0800

    Update PollingInput.h

--- src/internal/PollingInput.h
+++ src/internal/PollingInput.h
@@ -12,30 +12,9 @@ namespace DcsBios {
 	class PollingInput {
 		private:
 			virtual void pollInput();
-			int colCount = 0;
-			int rowCount = 0;
 		public:
 			static PollingInput* arrayInputs[][];
 			static void setMessageSentOrQueued() { messageSentOrQueued = true; }
-			static void pollInputs() {
-				for (int colIndex=0; colIndex < colCount; colIndex++) {
-					// col: set to output to low
-					byte curCol = cols[colIndex];
-					pinMode(curCol, OUTPUT);
-					digitalWrite(curCol, LOW);
-
-					// row: interate through the rows
-					for (int rowIndex=0; rowIndex < rowCount; rowIndex++) {
-						byte rowCol = rows[rowIndex];
-						pinMode(rowCol, INPUT_PULLUP);
-						arrayInputs[colIndex][rowIndex]->pollInput(digitalRead(rowCol));
-						pinMode(rowCol, INPUT);
-						
-					}
-					// disable the column
-					pinMode(curCol, INPUT);
-	}
-			}
 	};
 	
 }
