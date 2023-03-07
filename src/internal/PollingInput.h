fa5452295deef15a6d9b6286b5f6e98cf6d55c4c
prashant <prash42135@gmail.com>
2023-03-06 22:14:01 -0800

    Update PollingInput.h

--- src/internal/PollingInput.h
+++ src/internal/PollingInput.h
@@ -12,38 +12,29 @@ namespace DcsBios {
 	class PollingInput {
 		private:
 			virtual void pollInput();
-			PollingInput* nextPollingInput;
+			int colCount = 0;
+			int rowCount = 0;
 		public:
-			static PollingInput* firstPollingInput;
+			static PollingInput* arrayInputs[][];
 			static void setMessageSentOrQueued() { messageSentOrQueued = true; }
-			PollingInput() {
-				this->nextPollingInput = firstPollingInput;
-				firstPollingInput = this;
-			}
 			static void pollInputs() {
-				PollingInput* pi = firstPollingInput;
-				if (!pi) return;
-				
-				PollingInput* lastSender = NULL;
-				do { // step through circular linked list
-					messageSentOrQueued = false;
-					pi->pollInput();
-					if (messageSentOrQueued) {
-						lastSender = pi;
+				for (int colIndex=0; colIndex < colCount; colIndex++) {
+					// col: set to output to low
+					byte curCol = cols[colIndex];
+					pinMode(curCol, OUTPUT);
+					digitalWrite(curCol, LOW);
+
+					// row: interate through the rows
+					for (int rowIndex=0; rowIndex < rowCount; rowIndex++) {
+						byte rowCol = rows[rowIndex];
+						pinMode(rowCol, INPUT_PULLUP);
+						arrayInputs[colIndex][rowIndex]->pollInput(digitalRead(rowCol));
+						pinMode(rowCol, INPUT);
+						
 					}
-					
-					// make linked list circular if it is not already
-					if (pi->nextPollingInput == NULL) pi->nextPollingInput = firstPollingInput;
-					
-					pi = pi->nextPollingInput;
-				} while (pi != firstPollingInput); // util we get back to the start
-				
-				if (lastSender && (firstPollingInput != pi)) {
-					// if at least two elements in the list and someone queued
-					// or send a message, rotate the list to make the next poll
-					// start after the input that got to queue the last message
-					firstPollingInput = lastSender->nextPollingInput;
-				}
+					// disable the column
+					pinMode(curCol, INPUT);
+	}
 			}
 	};
 	
